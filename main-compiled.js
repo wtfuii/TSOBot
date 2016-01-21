7e0cf04f8b9055d7b158c77f7de8847726d826a3
Klaus Deja <klaus.deja@prodeus.com>
2016-01-21 22:32:38 +0100

    It works a little...

--- main-compiled.js
+++ main-compiled.js
@@ -1,115 +1,139 @@
-"use strict";
+'use strict';
 
-var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+function TeamSpeakListener() {
+  'use strict';
 
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
+  var TeamSpeakClient = require("node-teamspeak");
+  var TelegramBot = require('node-telegram-bot-api');
+  var mongoose = require('mongoose');
+  var fs = require('fs');
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+  var parsedJson = JSON.parse(fs.readFileSync('settings.json', 'utf8'));
+  var server = parsedJson.server;
+  var port = parsedJson.port;
+  var queryPort = parsedJson.queryPort;
 
-var TeamSpeakListener = function () {
-  function TeamSpeakListener(server, port, queryPort) {
-    _classCallCheck(this, TeamSpeakListener);
+  var cl = new TeamSpeakClient(server, queryPort);
 
-    var TeamSpeakClient = require("node-teamspeak");
-    this.server = server;
-    this.port = port;
-    this.queryPort = port;
-    this.cl = new TeamSpeakClient(server, queryPort);
-    this.serverNotifyRegister();
-  }
+  var bot = new TelegramBot(parsedJson.botApiKey, { polling: true });
 
-  _createClass(TeamSpeakListener, [{
-    key: "serverNotifyRegister",
-    value: function serverNotifyRegister() {
-      var _this = this;
+  var usersSchema = new mongoose.Schema({
+    "tgUserId": { type: String, index: true },
+    "notifyAll": { type: Boolean, default: false },
+    "notifyOnDisconnect": { type: Boolean, default: false },
+    "tsUserNames": [String]
+  });
+  var Users = mongoose.model('Users', usersSchema);
+  mongoose.connect(parsedJson.mongooseConnection);
+  var db = mongoose.connection;
+  db.on('error', console.error.bind(console, 'connection error:'));
 
-      this.cl.send("use", { port: this.port }, function () {
-        _this.cl.send("servernotifyregister", { event: "server" }, function () {});
+  db.once('open', function () {
+    bot.onText(/\/start/, function (msg) {
+      Users.findOneAndUpdate({ tgUserId: msg.from.id }, { tgUserId: msg.from.id }, { upsert: true }, function (err, result) {
+        if (result) {
+          console.log(result);
+          bot.sendMessage(msg.from.id, 'Welcome to the TSOBot');
+        }
+        if (err) {
+          bot.sendMessage(msg.from.id, 'Failed to Register your Telegram Account.');
+        }
       });
-    }
-  }, {
-    key: "handleClientEnterView",
-    value: function handleClientEnterView(bot, Users) {
-      this.cl.on("cliententerview", function (resp) {
-        //noinspection JSUnresolvedVariable
-        Users.find({ $or: [{ tsUserNames: resp.client_nickname }, { notifyAll: true }] }, function (err, result) {
-          if (result) {
-            for (var user in result) {
-              if (result.hasOwnProperty(user)) {
-                bot.sendMessage(user.tgUserId, "${resp.client_nickname} joined the server.");
-              }
-            }
-          }if (err) {
-            return;
-          }
-        });
-      });
-    }
-  }]);
-
-  return TeamSpeakListener;
-}();
-
-var TSOBot = exports.TSOBot = function () {
-  function TSOBot() {
-    _classCallCheck(this, TSOBot);
-
-    var TelegramBot = require('node-telegram-bot-api');
-    var mongoose = require('mongoose');
-    var fs = require('fs');
-
-    var parsedJson = JSON.parse(fs.readFileSync('settings.json', 'utf8'));
-
-    this.bot = new TelegramBot(parsedJson.botApiKey, { polling: true });
-    this.tsListener = new TeamSpeakListener(parsedJson.server, parsedJson.port, parsedJson.queryPort);
-
-    var usersSchema = new mongoose.Schema({
-      "tgUserId": { type: String, index: true },
-      "notifyAll": { type: Boolean, default: false },
-      "notifyOnDisconnect": { type: Boolean, default: false },
-      "tsUserNames": [String]
     });
-    var Users = mongoose.model('Users', usersSchema);
-    mongoose.connect(parsedJson.mongooseConnection);
-    var db = mongoose.connection;
-    db.on('error', console.error.bind(console, 'connection error:'));
-    db.once('open', this.setMessageListeners(Users));
 
-    this.tsListener.handleClientEnterView(this.bot, Users);
+    bot.onText(/\/subscribeall/, function (msg) {
+      Users.findOneAndUpdate({ tgUserId: msg.from.id }, { notifyAll: true }, function (err, result) {
+        if (result) {
+          bot.sendMessage(msg.from.id, 'You will receive a notification if any user joins the server.');
+        }
+        if (err) {
+          bot.sendMessage(msg.from.id, 'Failed to subscribe.');
+        }
+      });
+    });
+
+    bot.onText(/\/unsubscribeall/, function (msg) {
+      Users.findOneAndUpdate({ tgUserId: msg.from.id }, { notifyAll: false }, function (err, result) {
+        if (result) {
+          bot.sendMessage(msg.from.id, 'You will only receive notifications for users you specified.');
+        }
+        if (err) {
+          bot.sendMessage(msg.from.id, 'Failed to subscribe.');
+        }
+      });
+    });
+
+    bot.onText(/\/subscribe (.+)/, function (msg, match) {
+      Users.findOneAndUpdate({ tgUserId: msg.from.id }, { $addToSet: { tsUserNames: match[1] } }, function (err, result) {
+        if (result) {
+          bot.sendMessage(msg.from.id, 'You will receive a notification once ' + match[1] + ' joins the server.');
+        }
+        if (err) {
+          bot.sendMessage(msg.from.id, 'Failed to subscribe.');
+        }
+      });
+    });
+
+    bot.onText(/\/unsubscribe (.+)/, function (msg, match) {
+      Users.findOneAndUpdate({ tgUserId: msg.from.id }, { $pull: { tsUserNames: match[1] } }, function (err, result) {
+        if (result) {
+          bot.sendMessage(msg.from.id, 'You won\'t receive a notification once ' + match[1] + ' joins the server.');
+        }
+        if (err) {
+          bot.sendMessage(msg.from.id, 'Failed to unsubscribe.');
+        }
+      });
+    });
+  });
+
+  bot.onText(/\/subscriptions/, function (msg) {
+    Users.findOne({ tgUserId: msg.from.id }, function (err, result) {
+      if (result) {
+        (function () {
+          var subscriptions = "";
+          result.tsUserNames.forEach(function (element) {
+            subscriptions = subscriptions + element + "\n";
+          });
+          if (result.notifyAll) {
+            bot.sendMessage(msg.from.id, 'You subscribed to all users.');
+          } else if (subscriptions) {
+            bot.sendMessage(msg.from.id, 'You subscribed to: \n' + subscriptions);
+          } else {
+            bot.sendMessage(msg.from.id, 'You don\'t have any subscription.');
+          }
+        })();
+      }
+      if (err) {
+        bot.sendMessage(msg.from.id, 'Request failed.');
+      }
+    });
+  });
+
+  serverNotifyRegister();
+  handleClientEnterView();
+
+  function serverNotifyRegister() {
+    cl.send("use", { port: 9987 }, function () {
+      cl.send("servernotifyregister", { event: "server" }, function () {});
+    });
   }
 
-  _createClass(TSOBot, [{
-    key: "setMessageListeners",
-    value: function setMessageListeners(Users) {
-      var _this2 = this;
-
-      this.bot.onText(/\/start/, function (msg) {
-        Users.findOneAndUpdate({ tgUserId: msg.from.id }, { upsert: true }, function (err, result) {
-          if (result) {
-            _this2.bot.sendMessage(msg.from.id, 'Welcome to the TSOBot');
-          }
-          if (err) {
-            _this2.bot.sendMessage(msg.from.id, 'Failed to Register your Telegram Account.');
-          }
-        });
+  function handleClientEnterView() {
+    cl.on("cliententerview", function (resp) {
+      //noinspection JSUnresolvedVariable
+      Users.find({ $or: [{ tsUserNames: resp.client_nickname }, { notifyAll: true }] }).exec(function (err, result) {
+        if (err) {
+          return;
+        }if (result) {
+          result.forEach(function (element) {
+            bot.sendMessage(element.tgUserId, resp.client_nickname + " joined the server.");
+          });
+        }
       });
+    });
+  }
+}
 
-      this.bot.onText(/\/subscribeall/, function (msg) {
-        Users.findOneAndUpdate({ TgUserId: msg.from.id }, { "notifyAll": true }, function (err, result) {
-          if (result) {
-            _this2.bot.sendMessage(msg.from.id, 'You will receive a notification if any user connects to the server.');
-          }
-          if (err) {
-            _this2.bot.sendMessage(msg.from.id, 'Failed to subscribe.');
-          }
-        });
-      });
-    }
-  }]);
-
-  return TSOBot;
-}();
+new TeamSpeakListener();
 
 //# sourceMappingURL=main-compiled.js.map
--- main.js
+++ main.js
@@ -1,88 +1,138 @@
-class TeamSpeakListener {
-  constructor(server, port, queryPort) {
-    const TeamSpeakClient = require("node-teamspeak");
-    this.server = server;
-    this.port = port;
-    this.queryPort = port;
-    this.cl = new TeamSpeakClient(server, queryPort);
-    this.serverNotifyRegister();
-  }
+function TeamSpeakListener() {
+  'use strict';
+  const TeamSpeakClient = require("node-teamspeak");
+  const TelegramBot = require('node-telegram-bot-api');
+  const mongoose = require('mongoose');
+  const fs = require('fs');
 
-  serverNotifyRegister() {
-    this.cl.send("use", {port: this.port}, () => {
-      this.cl.send("servernotifyregister", {event: "server"}, () => {
-      });
-    });
-  }
+  const parsedJson = JSON.parse(fs.readFileSync('settings.json', 'utf8'));
+  const server = parsedJson.server;
+  const port = parsedJson.port;
+  const queryPort = parsedJson.queryPort;
 
-  handleClientEnterView(bot, Users) {
-    this.cl.on("cliententerview", (resp) => {
-      //noinspection JSUnresolvedVariable
-      Users.find({$or: [{tsUserNames: resp.client_nickname}, {notifyAll: true}]}, (err, result) => {
+  const cl = new TeamSpeakClient(server, queryPort);
+
+  const bot = new TelegramBot(parsedJson.botApiKey, {polling: true});
+
+  const usersSchema = new mongoose.Schema(
+    {
+      "tgUserId": {type: String, index: true},
+      "notifyAll": {type: Boolean, default: false},
+      "notifyOnDisconnect": {type: Boolean, default: false},
+      "tsUserNames": [String]
+    }
+  );
+  const Users = mongoose.model('Users', usersSchema);
+  mongoose.connect(parsedJson.mongooseConnection);
+  const db = mongoose.connection;
+  db.on('error', console.error.bind(console, 'connection error:'));
+
+  db.once('open', () => {
+    bot.onText(/\/start/, (msg) => {
+      Users.findOneAndUpdate({tgUserId: msg.from.id}, {tgUserId: msg.from.id}, {upsert: true}, (err, result) => {
         if (result) {
-          for (let user in result) {
-            if (result.hasOwnProperty(user)) {
-              bot.sendMessage(user.tgUserId, "${resp.client_nickname} joined the server.");
-            }
-          }
-        } if (err) {
-          return;
+          console.log(result);
+          bot.sendMessage(msg.from.id, 'Welcome to the TSOBot');
+        }
+        if (err) {
+          bot.sendMessage(msg.from.id, 'Failed to Register your Telegram Account.');
         }
       });
     });
-  }
-}
 
-export class TSOBot {
-  constructor() {
-    const TelegramBot = require('node-telegram-bot-api');
-    const mongoose = require('mongoose');
-    const fs = require('fs');
+    bot.onText(/\/subscribeall/, (msg) => {
+      Users.findOneAndUpdate({tgUserId: msg.from.id}, {notifyAll: true}, (err, result) => {
+        if (result) {
+          bot.sendMessage(msg.from.id, 'You will receive a notification if any user joins the server.');
+        }
+        if (err) {
+          bot.sendMessage(msg.from.id, 'Failed to subscribe.');
+        }
+      });
+    });
 
-    const parsedJson = JSON.parse(fs.readFileSync('settings.json', 'utf8'));
+    bot.onText(/\/unsubscribeall/, (msg) => {
+      Users.findOneAndUpdate({tgUserId: msg.from.id}, {notifyAll: false}, (err, result) => {
+        if (result) {
+          bot.sendMessage(msg.from.id, 'You will only receive notifications for users you specified.');
+        }
+        if (err) {
+          bot.sendMessage(msg.from.id, 'Failed to subscribe.');
+        }
+      });
+    });
 
-    this.bot = new TelegramBot(parsedJson.botApiKey, {polling: true});
-    this.tsListener = new TeamSpeakListener(parsedJson.server, parsedJson.port, parsedJson.queryPort);
+    bot.onText(/\/subscribe (.+)/, (msg, match) => {
+      Users.findOneAndUpdate({tgUserId: msg.from.id}, {$addToSet: {tsUserNames: match[1]}}, (err, result) => {
+        if(result) {
+          bot.sendMessage(msg.from.id, 'You will receive a notification once ' + match[1] + ' joins the server.');
+        }
+        if (err) {
+          bot.sendMessage(msg.from.id, 'Failed to subscribe.');
+        }
+      });
+    });
 
-    const usersSchema = new mongoose.Schema(
-      {
-        "tgUserId": {type: String, index: true},
-        "notifyAll": {type: Boolean, default: false},
-        "notifyOnDisconnect": {type: Boolean, default: false},
-        "tsUserNames": [String]
+    bot.onText(/\/unsubscribe (.+)/, (msg, match) => {
+      Users.findOneAndUpdate({tgUserId: msg.from.id}, {$pull: {tsUserNames: match[1]}}, (err, result) => {
+        if(result) {
+          bot.sendMessage(msg.from.id, 'You won\'t receive a notification once ' + match[1] + ' joins the server.');
+        }
+        if (err) {
+          bot.sendMessage(msg.from.id, 'Failed to unsubscribe.');
+        }
+      });
+    });
+  });
+
+  bot.onText(/\/subscriptions/, (msg) => {
+    Users.findOne({tgUserId: msg.from.id}, (err, result) => {
+      if(result) {
+        let subscriptions = "";
+        result.tsUserNames.forEach((element) => {
+          subscriptions = subscriptions + element + "\n";
+        });
+        if (result.notifyAll) {
+          bot.sendMessage(msg.from.id, 'You subscribed to all users.');
+        }
+        else if (subscriptions) {
+          bot.sendMessage(msg.from.id, 'You subscribed to: \n' + subscriptions);
+        } else {
+          bot.sendMessage(msg.from.id, 'You don\'t have any subscription.');
+        }
       }
-    );
-    const Users = mongoose.model('Users', usersSchema);
-    mongoose.connect(parsedJson.mongooseConnection);
-    const db = mongoose.connection;
-    db.on('error', console.error.bind(console, 'connection error:'));
-    db.once('open', this.setMessageListeners(Users));
+      if (err) {
+        bot.sendMessage(msg.from.id, 'Request failed.');
+      }
+    });
+  });
 
-    this.tsListener.handleClientEnterView(this.bot, Users);
-  }
 
-  setMessageListeners(Users) {
-    this.bot.onText(/\/start/, (msg) => {
-      Users.findOneAndUpdate({tgUserId: msg.from.id}, {upsert: true}, (err, result) => {
-        if (result) {
-          this.bot.sendMessage(msg.from.id, 'Welcome to the TSOBot');
-        }
-        if (err) {
-          this.bot.sendMessage(msg.from.id, 'Failed to Register your Telegram Account.');
-        }
+  serverNotifyRegister();
+  handleClientEnterView();
+
+  function serverNotifyRegister() {
+    cl.send("use", {port: 9987}, () => {
+      cl.send("servernotifyregister", {event: "server"}, () => {
       });
     });
+  }
 
-    this.bot.onText(/\/subscribeall/, (msg) => {
-      Users.findOneAndUpdate({TgUserId: msg.from.id}, {"notifyAll": true}, (err, result) => {
-        if (result) {
-          this.bot.sendMessage(msg.from.id, 'You will receive a notification if any user connects to the server.');
-        }
+  function handleClientEnterView() {
+    cl.on("cliententerview", (resp) => {
+      //noinspection JSUnresolvedVariable
+      Users.find({$or: [{tsUserNames: resp.client_nickname}, {notifyAll: true}]}).exec((err, result) => {
         if (err) {
-          this.bot.sendMessage(msg.from.id, 'Failed to subscribe.');
+          return;
+        } if (result) {
+          result.forEach((element) => {
+            bot.sendMessage(element.tgUserId, resp.client_nickname + " joined the server.");
+          });
         }
       });
     });
   }
 }
 
+
+new TeamSpeakListener();
